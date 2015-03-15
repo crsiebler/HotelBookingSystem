9f2a11b6ae53bb0e7265454d19af1532cf326580
Cory R Siebler <cory.siebler@gmail.com>
2015-03-14 22:49:24 -0700

    Removed uncessary files

---
+++ HotelBookingSystem/PriceCutEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// Name:   Cory Siebler
+/// ASUID:  1000832292
+/// Email:  csiebler@asu.edu
+/// Class:  ASU CSE 445 (#11845)
+namespace HotelBookingSystem
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class PriceCutEventArgs : EventArgs
+    {
+        private double price;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="price"></param>
+        public PriceCutEventArgs(double price)
+        {
+            this.Price = price;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+    }
+}
